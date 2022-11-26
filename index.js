baab3c4d0bf11b94fc4016d1f1ca7fcfa184af61
charlesdelab <aymericdelabrousse@gmail.com>
2022-11-26 18:02:36 +0100

    feat: domain, hash, path in mongo

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
 import mongoose from "mongoose";
-import Ingredients from "./ingredients.json" assert { type: "json" };
 import namehash from "@ensdomains/eth-ens-namehash";
-console.log(Ingredients.ingredients);
+import * as fs from "fs";
+import MerkleTree from "merkletreejs";
+import keccak256 from "keccak256";
+
+function loadIngredients(path) {
+  const rawdata = fs.readFileSync(path).toString().split(/\r?\n/);
+  rawdata.shift();
+  const data = rawdata.map((x) => {
+    return x.replace(/ /g, "");
+    //  + ".eth"
+  });
+  return data;
+}
+
+function computeNameHash(ingredients) {
+  return ingredients.map((x) => namehash.hash(x));
+}
+
+function computeMerkleTree(domains) {
+  const leaves = domains.map((x) => keccak256(x));
+  const tree = new MerkleTree.MerkleTree(leaves, keccak256);
+  const root = tree.getRoot().toString("hex");
+
+  // const proof = tree.getProof(leaves[0]);
+
+  const proofs = leaves.map((x) => tree.getHexProof(x));
+
+  // console.log(tree.verify(proof, leaves[0], root));
+
+  return { root: root, proofs: proofs };
+}
 
 (async () => {
-  console.log("Here!");
+  console.log("Inserting the data");
   await mongoose.connect("mongodb://localhost:27017/lfb");
-  console.log(mongoose.connection.readyState);
   const Ingredient = mongoose.model("Ingredient", {
     domain: "string",
     hash: "string",
+    path: "array",
   });
-  for (let ingredient of Ingredients.ingredients) {
-    var hash = namehash.hash(ingredient);
+  const domains = loadIngredients("./data/clean_ingredients.csv");
+  const hashes = computeNameHash(domains);
+  const merkle = computeMerkleTree(domains);
+  for (let i = 0; i < 10; i++) {
     const mongoIngredient = new Ingredient({
-      domain: ingredient.concat(".eth"),
-      hash: hash,
+      domain: domains[i].concat(".eth"),
+      hash: hashes[i],
+      path: merkle["proofs"][i],
     });
     await mongoIngredient.save();
   }
+  mongoose.connection.close();
+  console.log("Finished!");
 })();
